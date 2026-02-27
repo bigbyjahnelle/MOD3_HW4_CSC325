06d3ff40d552194cbaeb741ae9251f6c86f89adc
jahnellebigby <bigbjl@farmingdale.edu>
2026-02-27 15:42:04 -0500

    I completed Executive.java

--- src/main/java/com/mycompany/agency/Executive.java
+++ src/main/java/com/mycompany/agency/Executive.java
@@ -1,6 +1,6 @@
 package com.mycompany.agency;
 
-//Jahnelle to complete executive
+//Jahnelle to complete Executive
     // TODO 1: Make Executive a child of StaffEmploee
 public class Executive extends StaffEmployee
 {
